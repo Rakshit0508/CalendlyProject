3bcfd072605c31cf0c7eb8161573538607ceab90
Rakshit0508 <singhalrakshit0824@gmail.com>
2026-06-15 22:30:02 +0530

    Added get by id api for user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,14 @@
 import {Request, Response} from 'express';
-import { findAllUsers as findAllUsersService } from '../services/users.service.js';
+import { findAllUsers as findAllUsersService, findUserById as findUserByIdService} from '../services/users.service.js';
 
 export async function findAllusers(_req:Request, res:Response){
     const response= await findAllUsersService();
     res.json(response);
 
+}
+
+export async function findUserId(req:Request, res:Response){
+    const {id}= req.params;
+    const response= await findUserByIdService(Number(id));
+    res.json(response);
 }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { findAllusers } from "../controllers/user.controller.js";
+import { findAllusers, findUserId } from "../controllers/user.controller.js";
 
 
 export const userRouter: Router= Router();
 
 userRouter.get('/',findAllusers);
+
+userRouter.post('/:id',findUserId);
