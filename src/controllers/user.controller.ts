ff67b830f7f9d4551a66e698e03dbcb1a1569361
Rakshit0508 <singhalrakshit0824@gmail.com>
2026-06-26 14:27:00 +0530

    Added sendSuccess utitlity

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,23 +1,24 @@
 import {Request, Response} from 'express';
 import { createUserByNameAndEmail as createUserByNameAndEmailService, deleteUserByEmail as deleteUserByEmailService, findAllUsers as findAllUsersService, findUserById as findUserByIdService, updateUserByEmail as updateUserByEmailService} from '../services/users.service.js';
+import { sendSuccess } from '../utils/api-response.js';
 
 
 export async function findAllusers(_req:Request, res:Response){
     const response= await findAllUsersService();
-    res.json(response);
+    sendSuccess(res,response);
 
 }
 
 export async function findUserId(req:Request, res:Response){
     const {id}= req.params;
     const response= await findUserByIdService(Number(id));
-    res.json(response);
+    sendSuccess(res,response);
 }
 
 export async function createSingleUser(req:Request, res:Response){
     const {name,email}= req.body;
     const response= await createUserByNameAndEmailService(name,email);
-    res.json(response);
+    sendSuccess(res,response);
 }
 
 export async function deleteSingleUser(req:Request,res:Response){
