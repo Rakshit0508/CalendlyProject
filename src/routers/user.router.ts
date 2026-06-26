629755dfb6aeb772b6d7e7fc44a4652349f32788
Rakshit0508 <singhalrakshit0824@gmail.com>
2026-06-26 15:55:41 +0530

    Added zod validator for createuser

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createSingleUser, deleteSingleUser, findAllusers, findUserId, updateSingleUser } from "../controllers/user.controller.js";
+import { createUserSchema } from "../dtos/user.dto.js";
+import { validate } from "../middlewares/validate.js";
 
 
 export const userRouter: Router= Router();
 
 userRouter.get('/',findAllusers);
-userRouter.post("/create",createSingleUser);
+userRouter.post("/create",validate(createUserSchema),createSingleUser);
 userRouter.delete("/delete", deleteSingleUser);
 userRouter.put("/update",updateSingleUser);
 userRouter.get('/:id',findUserId);
