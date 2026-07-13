d91f34282894892f8de66b33d00352b169427b5f
Rakshit0508 <singhalrakshit0824@gmail.com>
2026-07-13 16:35:24 +0530

    Added env variables for temporal

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,7 @@ export const PORT= process.env.PORT || 3000;
 export const DATABASE_URL= process.env.DATABASE_URL || '';
 export const NODE_ENV= process.env.NODE_ENV || 'development';
 export const SLOT_GENERATION_DAYS= Number(process.env.SLOT_GENERATION_DAYS) || 30;
+
+export const TEMPORAL_ADDRESS= process.env.TEMPORAL_ADDRESS || 'temporal:7233'
+export const TEMPORAL_NAMESPACE= process.env.TEMPORAL_NAMESPACE || 'default'
+export const TEMPORAL_TASK_QUEUE= process.env.TEMPORAL_TASK_QUEUE || 'calendly-tasks'
